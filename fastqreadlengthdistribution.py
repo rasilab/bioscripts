01208ba6aa1016c5775cb2298655789d32e438f9
rasi <rasi1983@gmail.com>
2015-04-16 11:39:58 -0400

    read length distribution script added

---
+++ fastqreadlengthdistribution.py
@@ -0,0 +1,23 @@
+'''Returns a python dictionary with a key for each read length and
+a value corresponding to the number of reads of that length.
+Also prints the corresponding dictionary'''
+
+import itertools
+import gzip
+import sys
+
+myfile = sys.argv[1]
+
+def fastq_readlength_distribution( myfile ):
+    readcount = 0
+    readlengths = dict()
+    with gzip.open(files[0],"rb") as myfile:
+        for read in itertools.islice(myfile,1,None,4):
+            length = len(read.strip())
+            if length in readlengths:
+                readlengths[length] += 1
+            else:
+                readlengths[length] = 1
+            readcount += 1
+    print( "%d reads processed"%readcount )
+    print( readlengths )
